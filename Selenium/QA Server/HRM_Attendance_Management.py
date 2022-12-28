37a96cd6e35d4b7e0d5e01a7d80f450988cdb853
Anees Mufti <89216202+MuftiAnees@users.noreply.github.com>
2022-12-28 16:09:13 +0500

    Update HRM_Attendance_Management.py

--- Selenium/QA Server/HRM_Attendance_Management.py
+++ Selenium/QA Server/HRM_Attendance_Management.py
@@ -4,6 +4,8 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from time import sleep
 from CommonScripts.login import *
+from selenium.common.exceptions import NoSuchElementException
+
 
 PATH = "D:\Coding\Python\Selenium\chromedriver.exe"
 
@@ -15,5 +17,26 @@ sleep(2)
 
 driver.get("http://172.16.10.4:8082/jw/web/userview/HRMAttendanceMOdule/hrmUserview/_/8A4476F801BF4E4998A50FB480FB292D")
 print('HRM Attendance Management Opened')
+print(driver.title)
 sleep(2)
-print('Test Finished')
+
+try:
+    searchbar = driver.find_element(By.ID, 'st')
+except NoSuchElementException:
+    print('Searchbar Missing')
+
+
+search = driver.find_element(
+    By.XPATH, '//*[@id="category-container"]/li').click()
+sleep(2)
+search = driver.find_element(
+    By.XPATH, '//*[@id="D59C07C9D89C425C82EC4A06963CB6CA"]/a').click()
+
+print(driver.title)
+try:
+    searchbar = driver.find_element(By.ID, 'st')
+except NoSuchElementException:
+    print('Searchbar Missing')
+
+print('Test Completed')
+sleep(2)
